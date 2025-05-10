d2870d4827b4268360a71c8d0b3c7915a84a9998
Sawert <vladshimon2004@gmail.com>
2025-05-10 23:09:52 +0300

    create app

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -1,5 +1,5 @@
-// import { prisma } from '@/prisma/prisma-client';
-// import { NextRequest, NextResponse } from 'next/server';
+ import { prisma } from '@/prisma/prisma-client';
+ import { NextRequest, NextResponse } from 'next/server';
 
 // export async function GET(req: NextRequest) {
 //   try {
