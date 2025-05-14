06c33b3383c59a43cfc74e30cb217c01264e27a0
Sawert <vladshimon2004@gmail.com>
2025-05-14 17:52:44 +0300

    create app

--- shared/components/shared/modals/auth-modal/auth-modal.tsx
+++ shared/components/shared/modals/auth-modal/auth-modal.tsx
@@ -33,38 +33,7 @@ export const AuthModal: React.FC<Props> = ({ open, onClose }) => {
         )}
 
         <hr />
-        <div className="flex gap-2">
-          <Button
-            variant="secondary"
-            onClick={() =>
-              signIn('github', {
-                callbackUrl: '/',
-                redirect: true,
-              })
-            }
-            type="button"
-            className="gap-2 h-12 p-2 flex-1">
-            <img className="w-6 h-6" src="https://github.githubassets.com/favicons/favicon.svg" />
-            GitHub
-          </Button>
-
-          <Button
-            variant="secondary"
-            onClick={() =>
-              signIn('google', {
-                callbackUrl: '/',
-                redirect: true,
-              })
-            }
-            type="button"
-            className="gap-2 h-12 p-2 flex-1">
-            <img
-              className="w-6 h-6"
-              src="https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg"
-            />
-            Google
-          </Button>
-        </div>
+        
 
         <Button variant="outline" onClick={onSwitchType} type="button" className="h-12">
           {type !== 'login' ? 'Войти' : 'Регистрация'}
