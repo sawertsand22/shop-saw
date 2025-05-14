d3d43d296c16d9e0a6feb48418042cc34daea6a8
Sawert <vladshimon2004@gmail.com>
2025-05-14 14:02:20 +0300

    create app

---
+++ app/(root)/design-editor/page.tsx
@@ -0,0 +1,10 @@
+import { DesignEditor, Container } from '@/shared/components/shared/';
+
+export default function DesignEditorModalPage() {
+  return(
+
+    <Container className="flex flex-col my-10">
+          <DesignEditor/>
+        </Container>
+  );
+}
